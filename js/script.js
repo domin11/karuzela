2ed4107ee424c4b5049f8f9e7675184e45c30abe
Dominik Soboń <dominik.sobon@gmail.com>
2018-08-03 00:42:38 +0200

    Add progress bar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
-var restartButton = document.getElementById('restart');
+var restartButton = document.querySelector('.restart');
+var progressBar = document.querySelector('.progress-bar')
 
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
@@ -12,4 +13,9 @@ var flkty = new Flickity( elem, {
 
 restartButton.addEventListener( 'click', function() {
   flkty.selectCell('#malbork');
+});
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
 });
