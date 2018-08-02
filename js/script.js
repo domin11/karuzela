8a28ca7f7c79fef829a52c6f3467876c373e00f0
Dominik Soboń <dominik.sobon@gmail.com>
2018-08-03 00:29:02 +0200

    Add reload button

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+'use strict';
+var restartButton = document.getElementById('restart');
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
@@ -5,4 +8,8 @@ var flkty = new Flickity( elem, {
   contain: true,
   hash: true,
   pageDots: false
+});
+
+restartButton.addEventListener( 'click', function() {
+  flkty.selectCell('#malbork');
 });
